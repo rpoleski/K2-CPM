dae6565fb1641780fb0c05c1eaacbefbc8cda11b
Radek Poleski <radek.poleski@gmail.com>
2017-09-13 23:23:36 -0400

    apply_grids_and_prf() bjds=None option added

--- source/K2CPM/prfforcampaign.py
+++ source/K2CPM/prfforcampaign.py
@@ -7,12 +7,12 @@ from prfdata import PrfData
 class PrfForCampaign(object):
     """gives PRF values for a star and set of pixels in given (sub-)campaign"""
 
-    def __init__(self, campaign=None, grids=None, prfdata=None):
-        """grids must be of CampaignGridRaDec2Pix type and prfdata must be of PrfData type"""
+    def __init__(self, campaign=None, grids=None, prf_data=None):
+        """grids must be of CampaignGridRaDec2Pix type and prf_data must be of PrfData type"""
         if not isinstance(grids, CampaignGridRaDec2Pix):
             raise TypeError("wrong type of of grids argument ({:}), CampaignGridRaDec2Pix expected".format(type(grids)))
-        if not isinstance(prfdata, PrfData):
-            raise TypeError("wrong type of of prfdata argument ({:}), PrfData expected".format(type(prfdata)))
+        if not isinstance(prf_data, PrfData):
+            raise TypeError("wrong type of of prf_data argument ({:}), PrfData expected".format(type(prf_data)))
             
         if campaign is None:
             self.campaign = None
@@ -20,17 +20,18 @@ class PrfForCampaign(object):
             self.campaign = int(campaign)
 
         self.grids = grids
-        self.prfdata = prfdata
+        self.prf_data = prf_data
 
     def mean_position(self, ra, dec):
         """for given (RA,Dec) calculate position for every epoch
         and report weighted average"""
         return self.grids.mean_position(ra=ra, dec=dec)
 
-    def apply_grids_and_prf(self, ra, dec, pixels, bjds):
+    def apply_grids_and_prf(self, ra, dec, pixels, bjds=None):
         """For star at given (RA,Dec) try to calculate its positions for 
         epochs in bjds and for every epoch predict PRF value for every 
-        pixel in pixels (type: list).
+        pixel in pixels (type: list). If bjds is None than defaults to 
+        self.grids.bjd_array
         
         Returns:
           failed - number of epochs for which there are no grids
@@ -43,6 +44,9 @@ class PrfForCampaign(object):
         if not isinstance(ra, float) or not isinstance(dec, float):
             raise TypeError('wrong types of RA,Dec in apply_grids_and_prf(): {:} and {:}; 2 floats expected'.format(type(ra), type(dec)))
 
+        if bjds is None:
+            bjds = self.grids.bjd_array
+
         (positions_x, positions_y) = self.grids.apply_grids(ra=ra, dec=dec)
 
         n_epochs = len(bjds)
@@ -54,7 +58,7 @@ class PrfForCampaign(object):
             except:
                 mask[i] = False
                 continue
-            out_prfs[i] = self.prfdata.get_interpolated_prf(positions_x[index], positions_y[index], pixels)
+            out_prfs[i] = self.prf_data.get_interpolated_prf(positions_x[index], positions_y[index], pixels)
         
         self._positions_x = positions_x
         self._positions_y = positions_y
