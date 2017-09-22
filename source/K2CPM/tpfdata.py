52672b2128faf447e1a2c12e15dc771551618d05
Radek Poleski <radek.poleski@gmail.com>
2017-09-22 03:33:44 -0400

    additional check in tpfdata.py

--- source/K2CPM/tpfdata.py
+++ source/K2CPM/tpfdata.py
@@ -129,6 +129,8 @@ class TpfData(object):
 
     def check_pixel_covered(self, column, row):
         """check if we have data for given (column,row) pixel"""
+        if not isinstance(column, int) or not isinstance(row, int):
+            raise TypeError('Pixel coordinates must of int type')
         if not self.check_pixel_in_tpf(column, row):
             return False
         mask_value = self.mask[row - self.reference_row, column - self.reference_column]
